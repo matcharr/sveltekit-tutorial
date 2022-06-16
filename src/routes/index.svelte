7c05e074d90004d966b153428babad14fac50e1e
kyo <mathis.charretier.42@gmail.com>
2022-06-16 15:04:07 +0000

    update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  let title = "Ninja Gaming Guides";
+
+  const updateTitle = () => {
+    title = "something completely different";
+  };
+</script>
+
+<div class="index">
+  <h2>{title}</h2>
+  <p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, in
+    ratione quod libero voluptate saepe minus esse facere, eveniet fuga quas
+    sint eum blanditiis, molestias tempore deserunt id labore. Architecto.
+  </p>
+
+  <button on:click={updateTitle}>Change Title</button>
+  <input type="text" bind:value={title} />
+</div>
+
+<style>
+  .index {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
